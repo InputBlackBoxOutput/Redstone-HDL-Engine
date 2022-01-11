f8944fb32363c43ea8cf476896c45573f7c0742c
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2022-01-11 10:11:17 +0530

    CodeMirror - Code editor setup

--- www/index.js
+++ www/index.js
@@ -1,3 +1,10 @@
-const verilog_code = document.getElementById("verilog-code");
+const verilog_code = document.getElementById('verilog-code')
+verilog_code.value = "module m(a,b);\ninput a;\noutput b;\nwire a = ~b;\nendmodule"
 
-verilog_code.value = `module m(a,b);\ninput a;\noutput b;\nwire a = ~b;\nendmodule`;
+code = CodeMirror.fromTextArea(verilog_code, {
+    lineNumbers: true,
+    tabSize: 2,
+    mode: 'verilog',
+    theme: 'monokai'
+    });
+code.setSize(null, 605);
