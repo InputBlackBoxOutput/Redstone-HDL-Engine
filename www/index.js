c611b4138567abfcc0e67a0a3c6f401db465a0d5
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2022-01-12 11:43:34 +0530

    Minor changes

--- www/index.js
+++ www/index.js
@@ -1,5 +1,5 @@
 const verilog_code = document.getElementById('verilog-code')
-verilog_code.value = "module m(a,b);\ninput a;\noutput b;\nwire a = ~b;\nendmodule"
+verilog_code.value = "module m(a,b);\n\tinput a;\n\toutput b;\n\n\twire a = ~b;\nendmodule"
 
 code = CodeMirror.fromTextArea(verilog_code, {
     lineNumbers: true,
@@ -7,4 +7,8 @@ code = CodeMirror.fromTextArea(verilog_code, {
     mode: 'verilog',
     theme: 'monokai'
     });
-code.setSize(null, 605);
+code.setSize(null, 620);
+
+
+const synthesize_btn = document.getElementById("synthesize");
+synthesize_btn.disabled = true;
