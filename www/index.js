85a4e9990d8f062bf1528a4331679e6cd4b6f19b
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2022-01-13 10:55:29 +0530

    Diagram zoom in-out implemented

--- www/index.js
+++ www/index.js
@@ -12,3 +12,38 @@ code.setSize(null, 620);
 
 const synthesize_btn = document.getElementById("synthesize");
 synthesize_btn.disabled = true;
+
+
+var img = new Image();
+img.src = "images/dummy-diagram.png";
+
+function zoom() {
+    const canvas_div = document.getElementById('diagram-canvas-div');
+    const canvas = document.getElementById('diagram-canvas');
+    canvas.width = canvas_div.clientWidth;
+    canvas.height = canvas_div.clientHeight;
+
+    var ctx = canvas.getContext('2d');
+    var control = new CanvasManipulation(
+        canvas
+        , function () {draw(ctx)}
+    )
+    control.init();
+    control.layout();
+    draw(ctx);
+}
+
+
+function draw(ctx) {
+    ctx.clearCanvas();
+    ctx.drawImage(img, 0, 0);
+
+    // Display text on canvas
+    // ctx.beginPath()
+    // ctx.fillStyle = '#000'
+    // ctx.font = 'bold 80px Arial'
+    // ctx.fillText('Hello, world!', 50, 100)
+    // ctx.closePath()
+}
+
+window.onload = zoom;
