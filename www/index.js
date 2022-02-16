5a249dc6058dd37511caf9af5fdb5f0de4c2e5f7
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2022-02-16 22:13:25 +0530

    Handle mobile devices + Minor changes

--- www/index.js
+++ www/index.js
@@ -1,49 +1,63 @@
-const verilog_code = document.getElementById('verilog-code')
-verilog_code.value = "module m(a,b);\n\tinput a;\n\toutput b;\n\n\twire a = ~b;\nendmodule"
-
-code = CodeMirror.fromTextArea(verilog_code, {
-    lineNumbers: true,
-    tabSize: 2,
-    mode: 'verilog',
-    theme: 'monokai'
-    });
-code.setSize(null, 620);
-
-
+const verilog_code = document.getElementById("verilog-code");
+const canvas_div = document.getElementById("diagram-canvas-div");
+const canvas = document.getElementById("diagram-canvas");
 const synthesize_btn = document.getElementById("synthesize");
-synthesize_btn.disabled = true;
 
-
+// Preload image
 var img = new Image();
 img.src = "images/dummy-diagram.png";
 
+// Setup CodeMirror
+verilog_code.value =
+  "module m(a,b);\n\tinput a;\n\toutput b;\n\n\twire a = ~b;\nendmodule";
+
+code = CodeMirror.fromTextArea(verilog_code, {
+  lineNumbers: true,
+  tabSize: 2,
+  mode: "verilog",
+  theme: "monokai",
+});
+
+// Setup diagram zoom
 function zoom() {
-    const canvas_div = document.getElementById('diagram-canvas-div');
-    const canvas = document.getElementById('diagram-canvas');
-    canvas.width = canvas_div.clientWidth;
-    canvas.height = canvas_div.clientHeight;
+  canvas.hidden = false;
+  canvas.width = canvas_div.clientWidth;
+  canvas.height = canvas_div.clientHeight;
 
-    var ctx = canvas.getContext('2d');
-    var control = new CanvasManipulation(
-        canvas
-        , function () {draw(ctx)}
-    )
-    control.init();
-    control.layout();
-    draw(ctx);
+  var ctx = canvas.getContext("2d");
+  var control = new CanvasManipulation(
+    canvas,
+    function () {
+      draw(ctx);
+    },
+    { leftTop: { x: 0, y: 0 }, rightBottom: { x: 10000, y: 10000 } }
+  );
+  control.init();
+  control.layout();
+  draw(ctx);
 }
-
 
 function draw(ctx) {
-    ctx.clearCanvas();
-    ctx.drawImage(img, 0, 0);
+  ctx.clearCanvas();
+  ctx.drawImage(img, 0, 0);
 
-    // Display text on canvas
-    // ctx.beginPath()
-    // ctx.fillStyle = '#000'
-    // ctx.font = 'bold 80px Arial'
-    // ctx.fillText('Hello, world!', 50, 100)
-    // ctx.closePath()
+  // Display text on canvas
+  // ctx.beginPath()
+  // ctx.fillStyle = '#000'
+  // ctx.font = 'bold 80px Arial'
+  // ctx.fillText('Hello, world!', 50, 100)
+  // ctx.closePath()
 }
 
-window.onload = zoom;
+window.onload = () => {
+  // Enable diagram zoom
+  zoom();
+
+  // Resize code textarea
+  col_height = document.getElementById("verilog-code-div").clientHeight - 110;
+  console.log(col_height);
+  code.setSize(null, col_height);
+};
+
+// Synthesize
+synthesize_btn.disabled = true;
